0bc08b77563c588a203e5148824f49f83a5fcb2d
Riley <rjohnson@touchpadelectronics.com>
2022-02-08 12:17:52 -0600

    separated search to separate file

--- src/App.vue
+++ src/App.vue
@@ -7,34 +7,39 @@
           style="border: none; width: auto; font-size: 1.2rem; position: relative; z-index: 2000"
           class="d-inline"
           v-model="text"
-          v-on:keyup="Search"
+
           placeholder="Search">
       </b-form-input>
-      <b-card>
-<!--        <b-card-title style="width: 15rem">-->
-<!--          {{bestResult.name}}-->
-<!--        </b-card-title>-->
-<!--        <b-card-sub-title style="width: 15rem">-->
-<!--          {{bestResult.description}}-->
-<!--        </b-card-sub-title>-->
-<!--        <b-card-text style="width: 15rem">-->
-<!--          {{bestResult.address}}-->
-<!--        </b-card-text>-->
-<!--        <b-card-text style="width: 15rem">-->
-<!--          {{bestResult.review}}-->
-<!--        </b-card-text>-->
-<!--        <b-card-text style="width: 15rem">-->
-<!--          {{bestResult.type}}-->
-<!--        </b-card-text>-->
-      </b-card>
+      <b-row>
+        <b-col v-for="bestResult in this.bestResult" :key="bestResult.id">
+          <b-card>
+            <b-card-title style="width: 15rem">
+              {{bestResult.name}}
+            </b-card-title>
+            <b-card-sub-title style="width: 15rem; font-size: .4rem">
+              {{bestResult.description}}
+            </b-card-sub-title>
+            <b-card-text style="width: 15rem; font-size: .5rem">
+              {{bestResult.address}}
+            </b-card-text>
+            <b-card-text style="width: 15rem; font-size: .5rem">
+              {{bestResult.review}}
+            </b-card-text>
+            <b-card-text style="width: 15rem; font-size: .8rem">
+              {{bestResult.type}}
+            </b-card-text>
+          </b-card>
+        </b-col>
+      </b-row>
+
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
-import {stemmer} from 'stemmer';
-import stopwords from "@/stopwords";
+// import {stemmer} from 'stemmer';
+// import stopwords from "@/SearchAlgorithm/stopwords";
 import SearchAlgorithm from "@/SearchAlgorithm/SearchAlgorithm";
 
 export default {
@@ -47,129 +52,137 @@ export default {
       searchArray: [],
       text: null,
       bestResult: [],
+
+      searchAlgorithm: [],
     }
   },
   components: {},
 
+  watch: {
+    text: function (){
+      this.bestResult = this.searchAlgorithm.Search(this.text);
+    }
+  },
+
   methods: {
-    Search: function () {
-      let searchTerm = this.stemWords(this.text);
-      searchTerm = searchTerm.split(' ');
-      if (searchTerm.toString().length > 3) {
-        this.runComparison(searchTerm);
-      }
-    },
-
-    runComparison: function (searchTerm) {
-
-      let bestTerm;
-      let refDoc;
-      let highestTermScore = 0;
-      let previousTermScore = 0;
-      let refDocs = [];
-
-      console.log(searchTerm)
-
-      this.searchArray.forEach((arr) => {
-
-        arr.calculatedArray.forEach((term) => {
-
-          if(term[0].includes(searchTerm) && term[1] > highestTermScore){
-            highestTermScore = term[1];
-            bestTerm = term;
-            refDocs.push(arr);
-          }
-          else if(term[0].includes(searchTerm) && term[1] < highestTermScore && term[1] > previousTermScore){
-            previousTermScore = term[1];
-            refDocs.push(arr);
-          }
-        })
-      })
-
-
-      console.log(bestTerm);
-      console.log(refDoc)
-      this.bestResult = refDocs || null;
-
-    },
-
-
-    termFrequency: function (string) {
-      let temp = string.split(' ');
-      let returnArray = [];
-      let count = [];
-      temp.forEach((word) => {
-        let regex = new RegExp(word, 'g')
-        count = string.match(regex)
-        if (count.length !== 0 && !returnArray.includes(word)) {
-          count = [count[0], count.length];
-          returnArray.push(count)
-        }
-      })
-
-      returnArray.forEach((word) => {
-        word[1] = word[1] / returnArray.length;
-      })
-
-      return returnArray;
-    },
-
-    inverseDocumentFrequency: function (docArray) {
-      docArray.forEach((doc) => {
-        let idf;
-        let temp;
-        let count = 0;
-
-        doc.forEach((term) => {
-
-          temp = term[0]
-          docArray.forEach((doc) => {
-            if (doc.includes(temp)) {
-              count++;
-            }
-          })
-          idf = Math.log((docArray.length) / (1 + count))
-          term[1] = term[1] * idf;
-          // console.log(term)
-          let index = doc.indexOf(term)
-          docArray[docArray.indexOf(doc)][index] = term;
-          // console.log(docArray[docArray.indexOf(doc)][index])
-        })
-
-      })
-      return docArray;
-    },
-
-
-    truncate: function (string) {
-      string = this.stemWords(string);
-      string = this.removeStopWords(string);
-      string = this.termFrequency(string);
-
-      return string;
-    },
-
-    stemWords: function (string) {
-      let newString = string.replace(/[^a-zA-Z0-9 ]/g, '');
-      newString = string.split(/\s*[\s,.(){}+_=&^%$#@!'";:<>?/|\\-]\s*/);
-
-      let stemString = [];
-      newString.forEach((word) => {
-        stemString.push(stemmer(word));
-      })
-      return stemString.join(' ');
-    },
-
-    removeStopWords: function (string) {
-      let res = [];
-      let words = string.split(' ');
-      for (let i = 0; i < words.length; i++) {
-        if (!stopwords.includes(words[i])) {
-          res.push(words[i])
-        }
-      }
-      return res.join(' ');
-    },
+    // Search: function () {
+    //   let searchTerm = this.stemWords(this.text);
+    //   searchTerm = searchTerm.split(' ');
+    //   if (searchTerm.toString().length > 3) {
+    //     this.runComparison(searchTerm);
+    //   }
+    // },
+    //
+    // runComparison: function (searchTerm) {
+    //
+    //   let bestTerm;
+    //   let refDoc;
+    //   let highestTermScore = 0;
+    //   let previousTermScore = 0;
+    //   let refDocs = [];
+    //
+    //   console.log(searchTerm)
+    //
+    //   this.searchArray.forEach((arr) => {
+    //
+    //     arr.calculatedArray.forEach((term) => {
+    //
+    //       if(term[0].includes(searchTerm) && term[1] > highestTermScore){
+    //         highestTermScore = term[1];
+    //         bestTerm = term;
+    //         refDocs.push(arr);
+    //       }
+    //       else if(term[0].includes(searchTerm) && term[1] < highestTermScore && term[1] > previousTermScore){
+    //         previousTermScore = term[1];
+    //         refDocs.push(arr);
+    //       }
+    //     })
+    //   })
+    //
+    //
+    //   console.log(bestTerm);
+    //   console.log(refDoc)
+    //   this.bestResult = refDocs || null;
+    //
+    // },
+    //
+    //
+    // termFrequency: function (string) {
+    //   let temp = string.split(' ');
+    //   let returnArray = [];
+    //   let count = [];
+    //   temp.forEach((word) => {
+    //     let regex = new RegExp(word, 'g')
+    //     count = string.match(regex)
+    //     if (count.length !== 0 && !returnArray.includes(word)) {
+    //       count = [count[0], count.length];
+    //       returnArray.push(count)
+    //     }
+    //   })
+    //
+    //   returnArray.forEach((word) => {
+    //     word[1] = word[1] / returnArray.length;
+    //   })
+    //
+    //   return returnArray;
+    // },
+    //
+    // inverseDocumentFrequency: function (docArray) {
+    //   docArray.forEach((doc) => {
+    //     let idf;
+    //     let temp;
+    //     let count = 0;
+    //
+    //     doc.forEach((term) => {
+    //
+    //       temp = term[0]
+    //       docArray.forEach((doc) => {
+    //         if (doc.includes(temp)) {
+    //           count++;
+    //         }
+    //       })
+    //       idf = Math.log((docArray.length) / (1 + count))
+    //       term[1] = term[1] * idf;
+    //       // console.log(term)
+    //       let index = doc.indexOf(term)
+    //       docArray[docArray.indexOf(doc)][index] = term;
+    //       // console.log(docArray[docArray.indexOf(doc)][index])
+    //     })
+    //
+    //   })
+    //   return docArray;
+    // },
+    //
+    //
+    // truncate: function (string) {
+    //   string = this.stemWords(string);
+    //   string = this.removeStopWords(string);
+    //   string = this.termFrequency(string);
+    //
+    //   return string;
+    // },
+    //
+    // stemWords: function (string) {
+    //   let newString = string.replace(/[^a-zA-Z0-9 ]/g, '');
+    //   newString = string.split(/\s*[\s,.(){}+_=&^%$#@!'";:<>?/|\\-]\s*/);
+    //
+    //   let stemString = [];
+    //   newString.forEach((word) => {
+    //     stemString.push(stemmer(word));
+    //   })
+    //   return stemString.join(' ');
+    // },
+    //
+    // removeStopWords: function (string) {
+    //   let res = [];
+    //   let words = string.split(' ');
+    //   for (let i = 0; i < words.length; i++) {
+    //     if (!stopwords.includes(words[i])) {
+    //       res.push(words[i])
+    //     }
+    //   }
+    //   return res.join(' ');
+    // },
   },
 
   mounted() {
@@ -196,54 +209,60 @@ export default {
     }
 
     Promise.all(promises).then(() => {
+      // let searchAlgorithm = new SearchAlgorithm(this.array, ["name", 25], ["description", 10], ["address"], ["review"]);
+      // searchAlgorithm.Search('Orange', );
+      // searchAlgorithm.Search('Green');
+      // searchAlgorithm.Search('Blue');
 
-      let term;
+      // let term;
+      //
+      //
+      // this.array.forEach((item) => {
+      //
+      //   // let truncatedAddress = this.truncate(item.address);
+      //   // let truncatedDescription = this.truncate(item.description);
+      //   // let truncatedReview = this.truncate(item.review);
+      //
+      //
+      //   let concat = item.address.concat(item.description);
+      //   concat = concat.concat(item.review)
+      //   concat = concat.concat(item.name)
+      //   concat = concat.concat(item.type)
+      //
+      //   term = this.truncate(concat);
+      //   // let temp = this.truncate(item.description)
+      //   // termFrequency = termFrequency.concat(temp)
+      //   // temp = termFrequency.concat(this.truncate(item.review));
+      //   // termFrequency = termFrequency.concat(temp)
+      //
+      //
+      //   vm.truncatedArray.push(term);
+      //
+      // })
+      // vm.truncatedArray = this.inverseDocumentFrequency(vm.truncatedArray);
+      // for(let i = 0; i < vm.truncatedArray.length; i++){
+      //   vm.searchArray.push(
+      //       {
+      //         address: vm.array[i].address,
+      //         description: vm.array[i].description,
+      //         hours: vm.array[i].hours,
+      //         id: vm.array[i].id,
+      //         logo: vm.array[i].logo,
+      //         name: vm.array[i].name,
+      //         phone_number: vm.array[i].phone_number,
+      //         review: vm.array[i].review,
+      //         type: vm.array[i].type,
+      //         uid: vm.array[i].uid,
+      //         calculatedArray: vm.truncatedArray[i],
+      //       }
+      //   );
+      // }
 
+        this.searchAlgorithm = new SearchAlgorithm(this.array, ["name", 25], ["description", 10], ["address"], ["review"]);
 
-      this.array.forEach((item) => {
-
-        // let truncatedAddress = this.truncate(item.address);
-        // let truncatedDescription = this.truncate(item.description);
-        // let truncatedReview = this.truncate(item.review);
-
-
-        let concat = item.address.concat(item.description);
-        concat = concat.concat(item.review)
-        concat = concat.concat(item.name)
-        concat = concat.concat(item.type)
-
-        term = this.truncate(concat);
-        // let temp = this.truncate(item.description)
-        // termFrequency = termFrequency.concat(temp)
-        // temp = termFrequency.concat(this.truncate(item.review));
-        // termFrequency = termFrequency.concat(temp)
-
-
-        vm.truncatedArray.push(term);
-
-      })
-      vm.truncatedArray = this.inverseDocumentFrequency(vm.truncatedArray);
-      for(let i = 0; i < vm.truncatedArray.length; i++){
-        vm.searchArray.push(
-            {
-              address: vm.array[i].address,
-              description: vm.array[i].description,
-              hours: vm.array[i].hours,
-              id: vm.array[i].id,
-              logo: vm.array[i].logo,
-              name: vm.array[i].name,
-              phone_number: vm.array[i].phone_number,
-              review: vm.array[i].review,
-              type: vm.array[i].type,
-              uid: vm.array[i].uid,
-              calculatedArray: vm.truncatedArray[i],
-            }
-        );
-      }
     }).then(() => {
       // this.inverseDocumentFrequency();
-      let searchAlgorithm = new SearchAlgorithm();
-      searchAlgorithm.setDocumentReference(this.array, ["name", 25], ["description", 10], ["address"], ["review"]);
+
 
     })
 
--- src/SearchAlgorithm/SearchAlgorithm.js
+++ src/SearchAlgorithm/SearchAlgorithm.js
@@ -1,109 +1,248 @@
-// import {stemmer} from "stemmer";
-// import stopwords from "@/stopwords";
+import {stemmer} from "stemmer";
+import stopwords from "@/SearchAlgorithm/stopwords";
 
-function SearchAlgorithm() {
+function SearchAlgorithm(docArray = [], ...parameters) {
 
-    this.setDocumentReference = function (/**/) {
-        let args = Array.prototype.slice.call(arguments)
-        var obj = args[0];
 
-        let searchableStrings = [];
-        for (var i = 1; i < args.length; i++) {
-            if (Array.isArray(args[i])) {
-                searchableStrings.push({
-                    string: args[i][0],
-                    weight: args[i][1]
-                })
-            } else {
-                searchableStrings.push({
-                    string: args[i]
-                })
+    this.Search = function (searchTerm = "", maxReturnValue = 5){
+        searchTerm = stemWords(searchTerm);
+        searchTerm = searchTerm.split(' ');
+        let docRef = [];
+        if(searchTerm.toString().length > 0){
+            docRef = runComparison(searchTerm, maxReturnValue);
+        }
+        return docRef;
+    }
+
+    function runComparison(searchTerm = "", maxReturnValue = 5) {
+        let refDocs = [];
+        let bestTerms = [];
+
+        // console.log(searchTerm)
+
+        docArray.forEach((arr) => {
+
+            arr.searchIndex.forEach((term) => {
+
+                if(term[0].includes(searchTerm)){
+                    bestTerms.push({
+                        term: term,
+                        refDoc: arr,
+                    })
+
+                }
+            })
+        })
+
+        bestTerms.sort((a,b) => b.term[1] - a.term[1]);
+        console.log(bestTerms)
+        bestTerms.forEach((term)=>{
+            refDocs.push(term.refDoc)
+        })
+
+        return refDocs.slice(0, maxReturnValue) || null;
+
+    }
+
+    //Constructor parameters instantiated to usable properties
+    // let searchableArray = [];
+
+    let args = Array.prototype.slice.call(parameters)
+
+    if(!Array.isArray(docArray) || docArray.length <= 0){
+        throw new Error('docArray must be an array');
+    }
+    let parameterList = [];
+    for (let i = 0; i < args.length; i++) {
+        if (Array.isArray(args[i])) {
+            parameterList.push({
+                string: args[i][0],
+                weight: args[i][1]
+            })
+        } else {
+            parameterList.push({
+                string: args[i]
+            })
+        }
+    }
+    // console.log(searchableArray);
+
+    //Perform All Search Calculations
+    //truncate all parameters to remove excess information
+    truncateParameters();
+    //reduce searchable index to key->value pair
+    TF_IDF();
+    //return final calculated values to searchable string format
+    reformatSearchIndex();
+
+
+    // Check to make sure all parameters are correct and run truncate function
+    //on all document parameters
+    function truncateParameters(){
+        docArray.forEach((doc) => {
+            doc.searchIndex = [];
+            parameterList.forEach((parameter)=>{
+                if(typeof doc[parameter.string] === 'string' || doc[parameter.string] instanceof String){
+                    doc.searchIndex.push({
+                        string : truncate(doc[parameter.string]),
+                        weight : parameter.weight || null
+                    });
+                }
+                else{
+                    throw new Error('parameter must be a string');
+                }
+            })
+        })
+    }
+
+
+    // Helper method, runs stemWord and removeStopWord functions and
+    //returns result to truncateParameters to be pushed into the docArray searchIndex property
+    function truncate(string) {
+        string = stemWords(string);
+        string = removeStopWords(string);
+        console.log('variables truncated');
+        return string;
+    }
+
+    //uses stemmer MIT Javascript Library to return string with stemmed words
+    //https://github.com/words/stemmer
+    function stemWords (string) {
+        let newString = string.replace(/[^a-zA-Z0-9 ]/g, '');
+        newString = string.split(/\s*[\s,.(){}+_=&^%$#@!'";:<>?/|\\-]\s*/);
+
+        let stemString = [];
+        newString.forEach((word) => {
+            stemString.push(stemmer(word));
+        })
+        return stemString.join(' ');
+    }
+
+    //use imported stopword array to remove useless words
+    function removeStopWords (string) {
+        let res = [];
+        let words = string.split(' ');
+        for (let i = 0; i < words.length; i++) {
+            if (!stopwords.includes(words[i])) {
+                res.push(words[i])
             }
         }
-        console.log(obj);
-        console.log(searchableStrings)
+        return res.join(' ');
+    }
 
-        // let truncatedArrays = [];
-    };
+    //Helper method for calculating total (termFrequency * inverseDocumentFrequency) without weight
+    function TF_IDF(){
+        calculateTermFrequency();
+        console.log('calculated term frequency');
+        calculateInverseDocumentFrequency()
+        console.log('calculated inverse document frequency');
+        assignDocumentWeighting();
+        console.log('assigned document weighting');
+    }
 
+    function calculateTermFrequency(){
+        docArray.forEach((doc)=>{
+            doc.searchIndex.forEach((index)=>{
+                index.string = termFrequency(index.string);
+            })
+        })
+    }
 
+    function termFrequency(string) {
+        let temp = string.split(' ');
+        let returnArray = [];
+        let count = [];
+        temp.forEach((word) => {
+            let regex = new RegExp(word, 'g')
+            count = string.match(regex)
+            if (count.length !== 0 && !returnArray.includes(word)) {
+                count = [count[0], count.length];
+                returnArray.push(count)
+            }
+        })
 
+        returnArray.forEach((word) => {
+            word[1] = word[1] / returnArray.length;
+        })
 
-    // function truncate(string) {
-    //     string = stemWords(string);
-    //     string = removeStopWords(string);
-    //     string = termFrequency(string);
-    //
-    //     return string;
-    // }
-    //
-    // function stemWords (string) {
-    //     let newString = string.replace(/[^a-zA-Z0-9 ]/g, '');
-    //     newString = string.split(/\s*[\s,.(){}+_=&^%$#@!'";:<>?/|\\-]\s*/);
-    //
-    //     let stemString = [];
-    //     newString.forEach((word) => {
-    //         stemString.push(stemmer(word));
-    //     })
-    //     return stemString.join(' ');
-    // }
-    //
-    // function removeStopWords (string) {
-    //     let res = [];
-    //     let words = string.split(' ');
-    //     for (let i = 0; i < words.length; i++) {
-    //         if (!stopwords.includes(words[i])) {
-    //             res.push(words[i])
-    //         }
-    //     }
-    //     return res.join(' ');
-    // }
-    //
-    // function termFrequency(string) {
-    //     let temp = string.split(' ');
-    //     let returnArray = [];
-    //     let count = [];
-    //     temp.forEach((word) => {
-    //         let regex = new RegExp(word, 'g')
-    //         count = string.match(regex)
-    //         if (count.length !== 0 && !returnArray.includes(word)) {
-    //             count = [count[0], count.length];
-    //             returnArray.push(count)
-    //         }
-    //     })
-    //
-    //     returnArray.forEach((word) => {
-    //         word[1] = word[1] / returnArray.length;
-    //     })
-    //
-    //     return returnArray;
-    // }
-    //
-    // function inverseDocumentFrequency (docArray) {
-    //     docArray.forEach((doc) => {
-    //         let idf;
-    //         let temp;
-    //         let count = 0;
-    //
-    //         doc.forEach((term) => {
-    //
-    //             temp = term[0]
-    //             docArray.forEach((doc) => {
-    //                 if (doc.includes(temp)) {
-    //                     count++;
-    //                 }
-    //             })
-    //             idf = Math.log((docArray.length) / (1 + count))
-    //             term[1] = term[1] * idf;
-    //             // console.log(term)
-    //             let index = doc.indexOf(term)
-    //             docArray[docArray.indexOf(doc)][index] = term;
-    //             // console.log(docArray[docArray.indexOf(doc)][index])
-    //         })
-    //
-    //     })
-    //     return docArray;
-    // }
+        return returnArray;
+    }
+
+    function calculateInverseDocumentFrequency(){
+        let truncatedArray = [];
+        docArray.forEach((doc)=>{
+            truncatedArray.push(doc.searchIndex);
+        })
+        truncatedArray = inverseDocumentFrequency(truncatedArray);
+        for(let i = 0; i < truncatedArray.length; i++){
+            docArray[i].searchIndex = truncatedArray[i]
+        }
+    }
+
+    function inverseDocumentFrequency (docArray) {
+        docArray.forEach((doc) => {
+            let idf;
+            let temp;
+            let count = 0;
+
+            doc.forEach((stringArr) => {
+
+                stringArr.string.forEach((term)=>{
+                    temp = term[0]
+                    docArray.forEach((doc) => {
+                        doc.forEach((stringArr)=>{
+                            if(stringArr.string.toString().includes(temp)){
+                                count++;
+                            }
+                        })
+                        // if (doc.includes(temp)) {
+                        //     count++;
+                        // }
+                    })
+                    idf = (Math.log(1+ (docArray.length) / count))
+                    term[1] = term[1] * idf;
+                    // console.log(term)
+                    let index = stringArr.string.indexOf(term)
+                    docArray[docArray.indexOf(doc)][doc.indexOf(stringArr)].string[index] = term;
+                })
+
+            })
+
+        })
+        return docArray;
+    }
+
+    function assignDocumentWeighting(){
+        docArray.forEach((doc)=>{
+            doc.searchIndex.forEach((stringArr)=>{
+                let weight;
+                if(stringArr.weight != null || stringArr.weight !== 0){
+                    weight = stringArr.weight || 0;
+                }
+                else{
+                    weight = 0
+                }
+
+                stringArr.string.forEach((string)=>{
+                    string[1] = string[1] * (1 + (weight * .01));
+                })
+            })
+        })
+    }
+
+    function reformatSearchIndex(){
+        docArray.forEach((doc)=>{
+            let newStringArr = [];
+            doc.searchIndex.forEach((stringArr)=>{
+                stringArr.string.forEach((string)=>{
+                    newStringArr.push(string);
+                })
+
+            })
+            doc.searchIndex = newStringArr;
+        })
+        console.log('Ready to search!');
+    }
 
     return this;
 }
